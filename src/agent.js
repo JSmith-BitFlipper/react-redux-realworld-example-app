a2f0844c8f2ab5ad6356fb9fc0b314f89e1423c4
Damian Barabonkov <damianb@mit.edu>
2021-01-28 18:20:11 -0500

    Login takes username rather than email

--- src/agent.js
+++ src/agent.js
@@ -35,8 +35,8 @@ const requests = {
 const Auth = {
   current: () =>
     requests.get('/user'),
-  login: (email, password) =>
-    requests.post('/users/login', { user: { email, password } }),
+  login: (username, password) =>
+    requests.post('/users/login', { user: { username, password } }),
   register: (username, email, password) =>
     requests.post('/users', { user: { username, email, password } }),
   save: user =>
